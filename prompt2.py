4fb81b6ab2884a0ae5db6b7373c3a32d51b63524
kaitlinan <kahurd@ucsc.edu>
2024-10-03 14:22:59 -0700

    Add files via upload

---
+++ prompt2.py
@@ -0,0 +1,24 @@
+#Write a Python that prints the sum of two floating point numbers, the difference between two integers, and the product of a floating point number and an integer. In each case, have the program print out the data type of the resulting answer.
+
+x = 2.5
+y = 4.5
+z = (x+y)
+
+print(x+y) #prints the sum of two floating point numbers
+print(type(z))  #prints the data type of the sum (float)
+
+
+x = 20
+y = 5
+z = (x-y)
+
+print(x-y) #prints the difference between two integers
+print(type(z)) #prints the data type of the difference(int)
+
+
+x = 5.5
+y = 9
+z = (x*y)
+
+print(x*y) #prints the product between one float and one integer
+print(type(z)) #prints the data type of the product
