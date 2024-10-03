8b42667207e1d047e06058f5b3afd5954aa8877f
kaitlinan <kahurd@ucsc.edu>
2024-10-02 23:43:22 -0700

    Add files via upload

---
+++ Session-1-Prompt.py
@@ -0,0 +1,8 @@
+def AboutMe():
+	print("My name is Kaitlin Hurd and I use she/her pronouns. My favorite movie is Fantastic Mr. Fox. My favorite food is my Grandma's Chinese cooking.")
+
+def main():
+	AboutMe()
+
+if __name__ == '__main__':
+	main()
