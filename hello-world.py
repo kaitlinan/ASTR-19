aec94723d9a3dabd1a03febf054da9d189befbf5
kaitlinan <kahurd@ucsc.edu>
2024-10-02 23:33:17 -0700

    Add files via upload

---
+++ hello-world.py
@@ -0,0 +1,10 @@
+#hashtags are for comments
+
+def PrintHelloWorld(): #def defines a function. we are using the "print" function
+	print("Hello World")
+
+def main(): # the "main" function
+	PrintHelloWorld()
+
+if __name__=="__main__": #when we fun the function it will run this function first
+	main()
